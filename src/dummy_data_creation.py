016e1d4633528c18f74dad91bee08687f8428840
Karol Ortiz <87284674+Girlswhocodek@users.noreply.github.com>
2025-09-29 10:30:20 -0400

    Create dummy_data_creation.py

---
+++ src/dummy_data_creation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Creación de Datos Dummy con Distribución Normal Multivariada
+Ejercicio con NumPy y visualización con Matplotlib
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def problema_1_crear_randoms():
+    """
+    Problema 1: Crear 500 números aleatorios con distribución normal multivariada
+    Media: (-3, 0), Matriz de covarianza: [[1.0, 0.8], [0.8, 1.0]]
+    """
+    print("=" * 70)
+    print("PROBLEMA 1: CREACIÓN DE DATOS DUMMY")
+    print("=" * 
